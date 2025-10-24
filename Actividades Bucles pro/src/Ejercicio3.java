9ed9d59687068a51df29b6c3236ea55e4c4145e3
Nuria <236850070+Nuria1204@users.noreply.github.com>
2025-10-24 19:13:32 +0200

    Ejercicio3.java

--- Actividades Bucles pro/src/Ejercicio3.java
+++ Actividades Bucles pro/src/Ejercicio3.java
@@ -2,7 +2,19 @@ public class Ejercicio3 {
     static void main() {
 
     //Introducción
-          
+    String texto;
+    String invertida= "";       // sirve para guardar el texto al revés
+    Scanner sc= new Scanner(System.in);
 
+    //Desarrollo
+    System.out.print("Introduce una cadena: ");
+    texto= sc.nextLine();
+    for (int i= texto.length() - 1; i> 0; i--) {
+        invertida += texto.charAt(i);
+    }
+
+    //Final
+    System.out.print("La cadena invertida es: " + invertida);
     }
 }
+
