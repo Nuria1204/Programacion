2ccd110278fb3a4430b73877a557cdf42b6999eb
Nuria <236850070+Nuria1204@users.noreply.github.com>
2026-01-20 18:44:28 +0100

    Delete Ejercicio 1 CSV directory

--- Ejercicio 1 CSV/src/Ejecutadora.java
+++
@@ -1,7 +0,0 @@
-public class Ejecutadora {
-    static void main() {
-        String ruta = "C:\\Users\\1DAW\\IdeaProjects\\Ejercicios CSV\\src\\datos.csv";
-        String datos= "nombre, edad, ciudad";
-        Escritura.escritura(ruta,datos);
-    }
-}
