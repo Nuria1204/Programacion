adff7cffdce22c841d53ba7b796aebe47cddca0f
Nuria <236850070+Nuria1204@users.noreply.github.com>
2026-01-20 18:46:03 +0100

    Delete Ejercicio 9 CSV directory

--- Ejercicio 9 CSV/src/Ejecutar.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Ejecutar {
-    static void main() {
-        String origen = "C:\\Users\\1DAW\\IdeaProjects\\Ejercicio 4 CSV\\src\\datos 2.csv";
-        String destino = "C:\\Users\\1DAW\\IdeaProjects\\Ejercicio 9 CSV\\src\\ordenado.csv";
-        Leer.OrdenarPorPrimerCampo(origen, destino);
-        System.out.println("Archivo ordenado.csv creado correctamente");
-    }
-}
