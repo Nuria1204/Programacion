bb74c154beaeefee9bd590fc342231ad58416f6b
Nuria <236850070+Nuria1204@users.noreply.github.com>
2026-01-20 18:45:36 +0100

    Delete Ejercicio 5 CSV directory

--- Ejercicio 5 CSV/src/Ejecutar.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Ejecutar {
-    static void main() {
-        String ruta = "C:\\Users\\1DAW\\IdeaProjects\\Ejercicio 4 CSV\\src\\datos 2.csv";
-        Leer.contarlineas(ruta);
-    }
-}
